0f0902586a6c125b22be3bf65613e6943b0970a5
Douglas Volcato <douglasvolcato@gmail.com>
2023-05-27 11:40:34 -0300

    chore: change validation message

--- src/data/validators/book/validator.js
+++ src/data/validators/book/validator.js
@@ -2,7 +2,7 @@ module.exports = class BookValidator {
   validate(bookData) {
     switch (true) {
       case !bookData.title:
-        throw new Error("Missing book title");
+        throw new Error("missing required field title");
 
       default:
         return;
