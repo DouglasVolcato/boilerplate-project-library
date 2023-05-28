8e604aede75cbe4f96f3c416ae9031eb6c333a64
Douglas Volcato <douglasvolcato@gmail.com>
2023-05-27 23:00:43 -0300

    feat: add create book comment service

---
+++ src/data/services/comment/create.js
@@ -0,0 +1,24 @@
+module.exports = class CreateCommentService {
+  constructor(updateBookRepository, getBookRepository) {
+    this.updateBookRepository = updateBookRepository;
+    this.getBookRepository = getBookRepository;
+  }
+
+  async execute(bookId, commentData) {
+    const foundBook = await this.getBookRepository.execute(bookId);
+
+    if (!foundBook || (Array.isArray(foundBook) && foundBook.length === 0)) {
+      throw new Error("no book exists");
+    }
+
+    const newBook = {
+      _id: foundBook._id,
+      title: foundBook.title,
+      comments: [...foundBook.comments, commentData],
+    };
+
+    await this.updateBookRepository.execute(bookId, newBook);
+
+    return;
+  }
+};
